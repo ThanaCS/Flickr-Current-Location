8ad4935ee48e6cacc321d08377d7a3ebae400e13
ThanaCS <Thana_cs@hotmail.com>
2020-11-30 21:17:46 +0300

    Displaying info about photos

---
+++ app/src/main/java/com/thanaa/flickrcurrentlocation/DisplayFragment.kt
@@ -0,0 +1,38 @@
+package com.thanaa.flickrcurrentlocation
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_display.*
+
+
+class DisplayFragment(var photos: Photo, var url: String) : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_display, container, false)
+
+        //resizeImage(url)
+        return view
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        return super.onCreateDialog(savedInstanceState)
+    }
+
+    fun resizeImage(url: String) {
+        Glide.with(this)
+            .load(url)
+            .override(1024, 1024)
+            .into(img_preview)
+    }
+
+
+}
--- app/src/main/java/com/thanaa/flickrcurrentlocation/PhotoAdapter.kt
+++ app/src/main/java/com/thanaa/flickrcurrentlocation/PhotoAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.row_item.view.*
 
 class PhotoAdapter(private val photos: List<Photo>) : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
 
@@ -26,10 +28,18 @@ class PhotoAdapter(private val photos: List<Photo>) : RecyclerView.Adapter<Photo
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val photoItem = photos[position]
-        val url = "https://live.staticflickr.com/${photoItem.server}/${photoItem.id}_${photoItem.secret}.jpg"
+        val url =
+            "https://live.staticflickr.com/${photoItem.server}/${photoItem.id}_${photoItem.secret}.jpg"
         Log.d(TAG, "url")
         Glide.with(holder.itemImage).load(url).apply(RequestOptions().override(500, 500))
-                .centerCrop().into(holder.itemImage)
+            .centerCrop().into(holder.itemImage)
+
+        //when user clicks on item it navigate to update of the data position
+        holder.itemView.image_view.setOnClickListener { view ->
+            Toast.makeText(view.context, "${photos[position].title}", Toast.LENGTH_SHORT).show()
+
+        }
+
 
     }
 
