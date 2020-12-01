609e3bf17cb34ef984d123cf8097f21457ea6128
ThanaCS <Thana_cs@hotmail.com>
2020-12-01 18:33:54 +0300

    Set up google map fragment

--- app/src/main/java/com/thanaa/flickrcurrentlocation/ui/DisplayFragment.kt
+++ app/src/main/java/com/thanaa/flickrcurrentlocation/ui/DisplayFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -27,6 +28,10 @@ class DisplayFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         resizeImage(args.url)
+        location_text.setOnClickListener {
+            val action = DisplayFragmentDirections.actionDisplayFragmentToMapsFragment22()
+            findNavController().navigate(action)
+        }
     }
 
 
@@ -34,7 +39,6 @@ class DisplayFragment : Fragment() {
         Glide.with(this)
                 .load(url)
                 .apply(RequestOptions().override(1024, 1024))
-                // .override(1024, 1024)
                 .centerCrop()
                 .into(img_preview)
 
