77ff66295b8e4544ae82cbb71c15fd6eff654481
ThanaCS <Thana_cs@hotmail.com>
2020-12-02 06:11:57 +0300

    Set up SplashScreen and its assets

---
+++ app/src/main/java/com/thanaa/flickrcurrentlocation/ui/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.thanaa.flickrcurrentlocation.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.thanaa.flickrcurrentlocation.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
