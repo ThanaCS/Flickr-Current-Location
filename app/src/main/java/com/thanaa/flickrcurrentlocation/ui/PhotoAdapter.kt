8d433bee02a8f0aa69237ed43aa04cd78a4be80c
ThanaCS <Thana_cs@hotmail.com>
2020-12-04 20:50:13 +0300

    Separate the views from adapter to view holder and persist images in a local disk cache

--- app/src/main/java/com/thanaa/flickrcurrentlocation/ui/PhotoAdapter.kt
+++ app/src/main/java/com/thanaa/flickrcurrentlocation/ui/PhotoAdapter.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.thanaa.flickrcurrentlocation.R
 import com.thanaa.flickrcurrentlocation.model.Photo
 import com.thanaa.flickrcurrentlocation.util.toUrl
@@ -16,25 +18,63 @@ import kotlinx.android.synthetic.main.row_item.view.*
 
 private var TAG = "PhotoAdapter"
 var flag = false
+
 class PhotoAdapter(private val photos: List<Photo>) :
     RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
     lateinit var viewModel: FlickrViewModel
 
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val itemImage: ImageView = view.findViewById(R.id.image_view)
+        private val eyeOpenImage: ImageView = view.findViewById(R.id.eye_open)
+        private val eyeClosedImage: ImageView = view.findViewById(R.id.eye_close)
+        private val viewsNumberText: TextView = view.findViewById(R.id.view_number)
+        private val commentNumberText: TextView = view.findViewById(R.id.comment_number)
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val itemImage: ImageView = view.findViewById(R.id.image_view)
-        val eyeOpenImage: ImageView = view.findViewById(R.id.eye_open)
-        val eyeClosedImage: ImageView = view.findViewById(R.id.eye_close)
-        val viewsNumberText: TextView = view.findViewById(R.id.view_number)
-        val commentNumberText: TextView = view.findViewById(R.id.comment_number)
+        fun bind(url: String, views: String, comments: String) {
+            val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
+
+            Glide.with(itemImage)
+                .load(url)
+                .apply(requestOptions)
+                .centerCrop().into(itemImage)
+
+            viewsNumberText.text = views
+            commentNumberText.text = comments
+
+        }
+
+        fun eyeToggle(holder: ViewHolder) {
+            eyeClosedImage.setOnClickListener {
+                if (!flag) {
+                    holder.eyeClosedImage.visibility = View.GONE
+                    holder.eyeOpenImage.visibility = View.VISIBLE
+                    flag = true
+                } else {
+
+                    holder.eyeClosedImage.visibility = View.VISIBLE
+                    holder.eyeOpenImage.visibility = View.GONE
+                    flag = false
+                }
+            }
+            eyeOpenImage.setOnClickListener {
+                if (!flag) {
+                    holder.eyeClosedImage.visibility = View.GONE
+                    holder.eyeOpenImage.visibility = View.VISIBLE
+                    flag = true
+                } else {
+                    holder.eyeClosedImage.visibility = View.VISIBLE
+                    holder.eyeOpenImage.visibility = View.GONE
+                    flag = false
+                }
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
         )
-
     }
 
     override fun getItemCount() = photos.size
@@ -43,20 +83,18 @@ class PhotoAdapter(private val photos: List<Photo>) :
 
         viewModel = FlickrViewModel()
         val photoItem: Photo = photos[position]
+        val url = toUrl(photoItem.server, photoItem.id, photoItem.secret)
         viewModel.getGeoLocation(photoItem.id)
         viewModel.getPhotoInfo(photoItem.id)
-        val url = toUrl(photoItem.server, photoItem.id, photoItem.secret)
-        Glide.with(holder.itemImage)
-            .load(url)
-            .centerCrop().into(holder.itemImage)
 
         viewModel.photoInfoLiveData.observeForever {
-            holder.viewsNumberText.text = it.views
-            holder.commentNumberText.text = it.comments._content
-        }
-
-        //TODO:removeObserver
+            holder.bind(url, it.views, it.comments._content)
+            //TODO:removeObserver
 //            viewModel.photoInfoLiveData.removeObserver()
+        }
+        holder.eyeToggle(holder)
+
+        //passing data to DisplayFragment and navigating
         holder.itemView.image_view.setOnClickListener { view ->
             if (viewModel.locationLiveData.value != null) {
                 val action = LocationFragmentDirections.actionLocationFragmentToDisplayFragment(
@@ -68,34 +106,7 @@ class PhotoAdapter(private val photos: List<Photo>) :
             }
 
         }
-        eyeToggle(holder)
-    }
 
-    private fun eyeToggle(holder: ViewHolder) {
-
-        holder.eyeClosedImage.setOnClickListener {
-            if (!flag) {
-                holder.eyeClosedImage.visibility = View.GONE
-                holder.eyeOpenImage.visibility = View.VISIBLE
-                flag = true
-            } else {
-
-                holder.eyeClosedImage.visibility = View.VISIBLE
-                holder.eyeOpenImage.visibility = View.GONE
-                flag = false
-            }
-        }
-        holder.eyeOpenImage.setOnClickListener {
-            if (!flag) {
-                holder.eyeClosedImage.visibility = View.GONE
-                holder.eyeOpenImage.visibility = View.VISIBLE
-                flag = true
-            } else {
-                holder.eyeClosedImage.visibility = View.VISIBLE
-                holder.eyeOpenImage.visibility = View.GONE
-                flag = false
-            }
-        }
     }
 
 
