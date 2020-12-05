1ef333b21a5ac0ac2ea230d5c0b788492071074a
ThanaCS <Thana_cs@hotmail.com>
2020-12-05 06:39:48 +0300

    Add more test cases

--- app/src/test/java/com/thanaa/flickrcurrentlocation/viewmodel/FlickrViewModelTest.kt
+++ app/src/test/java/com/thanaa/flickrcurrentlocation/viewmodel/FlickrViewModelTest.kt
@@ -1,12 +1,9 @@
 package com.thanaa.flickrcurrentlocation.viewmodel
-
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.InjectMocks
-import org.mockito.MockitoAnnotations
 
 class FlickrViewModelTest {
     private lateinit var viewModel: FlickrViewModel
@@ -16,34 +13,43 @@ class FlickrViewModelTest {
 
     @Before
     fun setUp() {
-        @InjectMocks
         viewModel = FlickrViewModel()
-        MockitoAnnotations.initMocks(this)
-
     }
 
     @Test
-    fun `insert valid lan and long, returns result `() {
+    fun `insert valid lan and long , returns result `() {
         viewModel.getPhotos("24.8539523", "46.7133915")
-
         val value = viewModel.photosLiveData.getOrAwaitValue()
-        assertThat(value).isEqualTo(null)
+        print(value)
+        assertThat(value.isNotEmpty())
     }
 
     @Test
-    fun `insert valid lan and long d, returns result `() {
+    fun `insert valid lan and long test2, returns result `() {
         viewModel.getPhotos("0.0", "0.0")
+        val value = viewModel.photosLiveData.getOrAwaitValue()
+        print(value)
+        assertThat(value.isNotEmpty())
+    }
 
+    @Test
+    fun `insert empty lan and empty long, returns null `() {
+        viewModel.getPhotos("", "46.7133915")
         val value = viewModel.photosLiveData.getOrAwaitValue()
         assertThat(value).isEqualTo(null)
     }
 
     @Test
-    fun `insert empty lan and long, returns null `() {
+    fun `insert valid lan and empty long, returns null `() {
+        viewModel.getPhotos("46.7133915", "")
+        val value = viewModel.photosLiveData.getOrAwaitValue()
+        assertThat(value).isEqualTo(null)
+    }
 
+    @Test
+    fun `insert empty lan and valid long, returns null `() {
         viewModel.getPhotos("", "")
         val value = viewModel.photosLiveData.getOrAwaitValue()
-        // assertThat(value).isEqualTo(Experimental.Level.ERROR)
         assertThat(value).isEqualTo(null)
     }
 
@@ -61,6 +67,13 @@ class FlickrViewModelTest {
         assertThat(value).isEqualTo(null)
     }
 
+    @Test
+    fun `valid id in getGeoLocation(), returns result `() {
+        viewModel.getGeoLocation("50680435646")
+        val value = viewModel.locationLiveData.getOrAwaitValue()
+        print(value)
+        assertThat(value.region._content.isNotEmpty())
+    }
 
 }
 
